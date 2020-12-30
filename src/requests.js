12358616be7d21cfa366580fb78711efe3213de7
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2020-12-30 14:30:05 +0530

    initial commit

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+//api key from tmdb
+const API_KEY = "f3be645b2ec5e021c44c6e7547704f56";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated/api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests
