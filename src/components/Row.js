dc604cece5025ab202a379be99c2275df3f56828
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2020-12-30 20:55:41 +0530

    final app

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../axios'
 import './Row.css'
+import YouTube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({ title, fetchUrl, isLargeRow }) {
     //!movies state
     const [movies, setMovies] = useState([])
-
+    //?trailerUrl state
+    const [trailerUrl, setTrailerUrl] = useState("")
     //* A snippet of code which runs based on a specific condition/variable
     useEffect(() => {
         //* if [] , run once when the row loads &then don't run it again
@@ -21,6 +24,26 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData()
     }, [fetchUrl])
 
+    //?options for the youtube video trailers
+    const opts = {
+        height: "390",
+        width: "50%",
+        playerVars: {
+            autoplay: 1,
+        }
+    }
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('')
+        } else {
+
+            movieTrailer(movie?.title || "")
+                .then(url => {
+                    const urlParams = new URLSearchParams(new URL(url).search)
+                    setTrailerUrl(urlParams.get('v'))
+                }).catch((error) => console.log(error))
+        }
+    }
 
     return (
         <div className="row">
@@ -31,6 +54,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
                 {movies.map(movie => (
                     <img
+                        onClick={() => handleClick(movie)}
                         className={`row__poster ${isLargeRow && " row__posterLarge"}`}
                         key={movie.id}
                         src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -39,7 +63,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 ))}
 
             </div>
-
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
